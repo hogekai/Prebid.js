ef67c73e35c694e64d5be4c886212159451b2cd2
Kai Miyamoto <kaihogehoge@gmail.com>
2024-11-22 12:04:02 +0900

    Michao Bid Adapter: Implement validation of bid requests

--- modules/michaoBidAdapter.js
+++ modules/michaoBidAdapter.js
@@ -1,15 +1,46 @@
 import { registerBidder } from "../src/adapters/bidderFactory.js";
 import { BANNER, VIDEO } from "../src/mediaTypes.js";
+import { logError } from "../src/utils";
 
-const BIDDER_CODE = 'michao';
+const BIDDER_CODE = "michao";
 
 export const spec = {
-    code: BIDDER_CODE,
-    supportedMediaTypes: [BANNER, VIDEO],
-    isBidRequestValid: function(bid) {},
-    buildRequests: function(validBidRequests, bidderRequest) {},
-    interpretResponse: function(serverResponse, request) {},
-    getUserSyncs: function(syncOptions, serverResponses, gdprConsent, uspConsent) {},
+  code: BIDDER_CODE,
+  supportedMediaTypes: [BANNER, VIDEO],
+
+  isBidRequestValid: function (bid) {
+    if (typeof bid.params !== "object") {
+      return false;
+    }
+
+    const props = ["site", "placement"];
+    if (
+      props.some((prop) => {
+        bid.params[prop] = parseInt(bid.params[prop]);
+        return isNaN(bid.params[prop]);
+      })
+    ) {
+      domainLogger.bidRequestValidationError();
+      return false;
+    }
+
+    return true;
+  },
+
+  buildRequests: function (validBidRequests, bidderRequest) {},
+  interpretResponse: function (serverResponse, request) {},
+  getUserSyncs: function (
+    syncOptions,
+    serverResponses,
+    gdprConsent,
+    uspConsent
+  ) {},
+};
+
+export const domainLogger = {
+    bidRequestValidationError() {
+        logError("Michao: wrong format of site or placement.");
+    }
 };
 
 registerBidder(spec);
--- test/spec/modules/michaoBidAdapter_spec.js
+++ test/spec/modules/michaoBidAdapter_spec.js
@@ -1,3 +1,39 @@
-describe('Michao Bidder Adapter', () => {
+import { expect } from "chai";
+import { domainLogger, spec } from "../../../modules/michaoBidAdapter";
 
+describe("the michao bidder adapter", () => {
+  const bidRequestValidationTestCases = [
+    {
+      bid: { params: "non-object" },
+      expected: false,
+      shouldLogError: false,
+    },
+    {
+      bid: { params: { site: 123, placement: "456" } },
+      expected: true,
+      shouldLogError: false,
+    },
+    {
+      bid: { params: { site: "123", placement: "super-placement" } },
+      expected: false,
+      shouldLogError: true,
+    },
+  ];
+
+  bidRequestValidationTestCases.forEach((testCase) => {
+    it("validate bid requests", () => {
+      const validationErrorLogSpy = sinon.spy(
+        domainLogger,
+        "bidRequestValidationError"
+      );
+      const result = spec.isBidRequestValid(testCase.bid);
+
+      expect(result).to.equal(testCase.expected);
+      expect(validationErrorLogSpy.calledOnce).to.equal(
+        testCase.shouldLogError
+      );
+
+      validationErrorLogSpy.restore();
+    });
+  });
 });
